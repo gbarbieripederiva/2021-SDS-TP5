f5107ac92418b57537928977d4ee360a104b2fd8
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-06-03 14:00:49 -0300

    finished grapha

--- visualization/graphA.py
+++ visualization/graphA.py
@@ -1,24 +1,39 @@
 import os
-import numpy as np
 import matplotlib.pyplot as plt
 
-EXPERIMENT_DATA_PATH = os.path.join("data/experiment", "experimentA.txt")
+EXPERIMENT_DATA_PATH = os.path.join("..","data","experiment", "experimentA.txt")
 
 def parse(path):
     file = open(path, "r")
-    file.readline()
+    line = file.readline().strip().split(" ")
+    time_step = float(line[1])
     line = file.readline()
+    xs = []
+    ys = []
     while line:
         temp = list(map(float,line.strip().split(" ")))
-
+        t = 0
+        x = []
+        y = []
+        particles = 0
+        while t < temp[-1] + time_step:
+            if temp[particles] < t:
+                particles+=1
+            x.append(t)
+            y.append(particles)
+            t += time_step
+        xs.append(x)
+        ys.append(y)
         line = file.readline()
-    return
+    return xs,ys
 
 
-x,x = parse(EXPERIMENT_DATA_PATH)
+xs,ys = parse(EXPERIMENT_DATA_PATH)
 
-plt.plot(time, approximation, label=methods[i], linewidth=2)
+for i,_ in enumerate(xs):
+    plt.plot(xs[i], ys[i], label=f"Intento {i}", linewidth=2)
 plt.xlabel('Tiempo (s)', fontsize=18)
 plt.ylabel('Particulas Egresadas', fontsize=16)
 plt.legend(fontsize=16)
+plt.gcf().set_size_inches(16,12)
 plt.show()
