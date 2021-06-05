fffdd46ea73ccc891e9413a5c704e49dc1d70e48
Agustín Roca <aroca@itba.edu.ar>
2021-06-05 00:59:45 -0300

    ExperimentC done

--- simulation/ar/edu/itba/experiments/ExperimentC.java
+++ simulation/ar/edu/itba/experiments/ExperimentC.java
@@ -17,12 +17,12 @@ import ar.edu.itba.models.particle.Vector;
 
 public class ExperimentC {
     private static final int TRIES_TO_DO = 3;
-    private static final String OUTPUT_FILENAME = "./data/experiment/experimentC4.txt";
-    private static final double OPEN_SPACE = 3.0;
+    private static final String OUTPUT_FILENAME = "./data/experiment/experimentC.txt";
+    private static final double[] OPEN_SPACES = {1.2, 1.8, 2.4, 3.0};
     private static final double WALL_SIDE = 20;
     private static final double TARGET_WALL_GAP = 0.1;
     private static final double SECOND_TARGET_DISTANCE = 10;
-    private static final int PARTICLES_TO_GENERATE = 380;
+    private static final int[] PARTICLES_TO_GENERATE = {200, 260, 320, 380};
     private static final double MIN_RADIUS = 0.15 ;
     private static final double MAX_RADIUS = 0.32;
     private static final double TIME_TO_REACH_MAX_RADIUS = 0.5;
@@ -31,7 +31,6 @@ public class ExperimentC {
 
     private static final double TIME_STEP = 0.01;
 
-    // TODO:check
     private static final long SEED = System.currentTimeMillis();
     private static final Random RAND_GEN = new Random(SEED);
 
@@ -44,49 +43,52 @@ public class ExperimentC {
                 java.lang.System.exit(1);
             }
         }
-        BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(OUTPUT_FILENAME).toAbsolutePath().toString(), false));   
+        BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(OUTPUT_FILENAME).toAbsolutePath().toString(), false));
 
         writer.write("" + SEED + "-" + TIME_STEP);
-        
-        for (int tries = 0; tries < TRIES_TO_DO; tries++) {
-            writer.write("\n");
-            // Generate system and execute simulation
-            CPMSystem system = generateSystem();
-            long i = 0;
-            int pqty = system.particles.size();
-            int previousQty = pqty;
-            for(; system.particles.size() > 0; i++){
-                int currPqty = system.particles.size();
-                if(previousQty - currPqty > 0){
-                    writer.write("" + (system.deltaTime * i) + "-" + (pqty-currPqty) + " ");
-                    previousQty = currPqty;
+        for (int simulationNumber = 0; simulationNumber < OPEN_SPACES.length; simulationNumber++) {
+            writer.write("\n" + OPEN_SPACES[simulationNumber] + "-" + PARTICLES_TO_GENERATE[simulationNumber]);
+            for (int tries = 0; tries < TRIES_TO_DO; tries++) {
+                writer.write("\n");
+                // Generate system and execute simulation
+                CPMSystem system = generateSystem(OPEN_SPACES[simulationNumber], PARTICLES_TO_GENERATE[simulationNumber]);
+                long i = 0;
+                int pqty = system.particles.size();
+                int previousQty = pqty;
+                for (; system.particles.size() > 0; i++) {
+                    int currPqty = system.particles.size();
+                    if (previousQty - currPqty > 0) {
+                        writer.write("" + (system.deltaTime * i) + "-" + (pqty - currPqty) + " ");
+                        previousQty = currPqty;
+                    }
+                    system.simulateStep();
                 }
-                system.simulateStep();
+                System.out.println("tries:" + simulationNumber + "." + tries);
             }
-            System.out.println("tries:"+tries);
+            writer.write("\n");
         }
         // close files
         writer.close();
     }
 
-    private static CPMSystem generateSystem(){
+    private static CPMSystem generateSystem(double openSpace, int particlesToGenerate){
         // Add the walls in a counterclockwise way so isLeft stays false
         List<Wall> walls = new ArrayList<Wall>();
         walls.add(new Wall(new Vector(0,0), new Vector(0,WALL_SIDE)));
         walls.add(new Wall(new Vector(0,WALL_SIDE), new Vector(WALL_SIDE,WALL_SIDE)));
         walls.add(new Wall(new Vector(WALL_SIDE,WALL_SIDE), new Vector(WALL_SIDE,0)));
-        walls.add(new Wall(new Vector( (WALL_SIDE-OPEN_SPACE) / 2,0), new Vector(0,0)));
-        walls.add(new Wall(new Vector(WALL_SIDE,0), new Vector((WALL_SIDE+OPEN_SPACE)/2,0)));
+        walls.add(new Wall(new Vector( (WALL_SIDE-openSpace) / 2,0), new Vector(0,0)));
+        walls.add(new Wall(new Vector(WALL_SIDE,0), new Vector((WALL_SIDE+openSpace)/2,0)));
         
         // Add targets
         List<Wall> targets = new ArrayList<>();
-        targets.add(new Wall(new Vector((WALL_SIDE+OPEN_SPACE)/2 - TARGET_WALL_GAP, 0),new Vector((WALL_SIDE-OPEN_SPACE)/2 + TARGET_WALL_GAP, 0)));
+        targets.add(new Wall(new Vector((WALL_SIDE+openSpace)/2 - TARGET_WALL_GAP, 0),new Vector((WALL_SIDE-openSpace)/2 + TARGET_WALL_GAP, 0)));
         // TODO: check this values for second target
         targets.add(new Wall(new Vector(WALL_SIDE,- SECOND_TARGET_DISTANCE), new Vector(0,- SECOND_TARGET_DISTANCE)));
 
         // TODO: check this whole particle generation
         List<Particle> particles = new ArrayList<>();
-        for (int i = 0; i < PARTICLES_TO_GENERATE; i++) {
+        for (int i = 0; i < particlesToGenerate; i++) {
             particles.add(new Particle(
                 new ParticleRadius(MIN_RADIUS, MAX_RADIUS, TIME_TO_REACH_MAX_RADIUS, MAX_RADIUS), 
                 DESIRED_SPEED, 
--- visualization/graphC.py
+++ visualization/graphC.py
@@ -1,15 +1,15 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
-EXPERIMENT_DATA_PATH = os.path.join("data","experiment", "experimentC.txt")
+EXPERIMENT_DATA_PATH = os.path.join("..","data","experiment", "experimentC.txt")
 
 def parse(path):
     file = open(path, "r")
-    line = file.readline().strip().split(" ")
+    line = file.readline().strip().split("-")
+    dt = float(line[1])
     xs = []
     ys = []
-    x = []
-    y = []
     ds = []
     ns = []
 
@@ -21,28 +21,57 @@ def parse(path):
         n = int(title[1])
         ns.append(n)
         line = file.readline()
+        x_sim = []
+        y_sim = []
         while line!="\n" and line!='':
             temp = line.strip().split(" ")
+            x = []
+            y = []
             for j in temp:
                 aux = j.strip().split("-")
                 x.append(float(aux[0]))
                 y.append(int(aux[1]))
-            xs.append(x)
-            ys.append(y)
+            x_sim.append(x)
+            y_sim.append(y)
             line = file.readline()
+        xs.append(x_sim)
+        ys.append(y_sim)
     
-    return xs,ys,ds,ns
+    return dt, xs, ys, ds, ns
 
 
-xs,ys,ds,ns = parse(EXPERIMENT_DATA_PATH)
+def max_time(times):
+    max_t = 0
+    for iteration in times:
+        for t in iteration:
+            last_time = t[-1]
+            if last_time > max_t:
+                max_t = last_time
+    return np.ceil(max_t)
 
 
+dt, xs, ys, ds, ns = parse(EXPERIMENT_DATA_PATH)
+avg_x = np.arange(0, max_time(xs), 3)
+avg_y = []
+for i, (iteration_x, iteration_y) in enumerate(zip(xs, ys)):
+    print(i)
+    avg_y.append([])
+    for time in avg_x:
+        accum_y = 0
+        for sim_x, sim_y in zip(iteration_x, iteration_y):
+            prev_y = 0
+            for x, y in zip(sim_x, sim_y):
+                if np.ceil(x) == time:
+                    accum_y += y - prev_y
+                prev_y = y
+        accum_y /= len(iteration_y)
+        avg_y[i].append(accum_y)
 
-for i in range(len(xs)):
-    plt.plot(ys[i],xs[i], color='r' ,label=f"Intento {i}", linewidth=2)    
+for i in range(len(avg_y)):
+    plt.plot(avg_x, avg_y[i], label=f"N={ns[i]} d={ds[i]}", linewidth=2)
 
 plt.xlabel('Tiempo (s)', fontsize=18)
-plt.ylabel('Particulas Egresadas', fontsize=16)
+plt.ylabel('Caudal (1/s)', fontsize=16)
 plt.legend(fontsize=16)
 plt.gcf().set_size_inches(16,12)
 plt.show()
